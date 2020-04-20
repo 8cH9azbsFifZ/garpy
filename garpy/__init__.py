1e44d48f2e1a83f0469d34375082336d44a930ca
Gerolf Ziegenhain <g@ziegenhain.com>
2020-04-20 19:17:50 +0200

    copied pip stuff

--- garpy/__init__.py
+++ garpy/__init__.py
@@ -6,7 +6,6 @@ import sys
 from .activity import Activity, Activities
 from .client import GarminClient
 from .download import ActivitiesDownloader
-from .wellness import Wellness
 
 # Create logger
 logger = logging.getLogger(__name__)
